e367dc3f2f0f894fb4555653026bac1ae37fcb55
Yuri Franca dos Anjos <yuri.anjos@cwi.com.br>
2021-03-16 20:58:36 -0300

    api

--- api/src/main/java/br/com/ifsul/trabalhoSI/feature/CriptografiaSimetrica.java
+++ api/src/main/java/br/com/ifsul/trabalhoSI/feature/CriptografiaSimetrica.java
@@ -2,37 +2,38 @@ package br.com.ifsul.trabalhoSI.feature;
 
 
 import java.security.InvalidKeyException;
+import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CriptografiaSimetrica {
     public void metodo() {
         try {
-            KeyGenerator keygenerator = KeyGenerator.getInstance("DES");
-            SecretKey chaveSecreta = keygenerator.generateKey();
+            String senha="senhaDosGugu";
+            Key chaveSecreta =new SecretKeySpec(senha.getBytes(),0, 8, "DES");
+
+            byte[] textoBytes = "Exemplo de texto puro".getBytes();
+            String textoNormal = new String(textoBytes);
 
             // Cria a forma de criptografia
             Cipher cifraDES= Cipher.getInstance("DES/ECB/PKCS5Padding");
 
-            byte[] textoBytes = "Exemplo de texto puro".getBytes();
-
             // Inicializa a cifra para o processo de encriptação
             cifraDES.init(Cipher.ENCRYPT_MODE, chaveSecreta);
-            // Texto encriptado
-            byte[] textoEncriptado = cifraDES.doFinal(textoBytes);
+            byte[] textoEncriptadoBytes = cifraDES.doFinal(textoBytes);
+            String textoEncriptado=new String(textoEncriptadoBytes);
 
             // Inicializa a cifra também para o processo de decriptação
             cifraDES.init(Cipher.DECRYPT_MODE, chaveSecreta);
-            // Decriptografa o texto
-            byte[] textoDecriptografado = cifraDES.doFinal(textoEncriptado);
-
+            byte[] textoDecriptografadoBytes = cifraDES.doFinal(textoEncriptadoBytes);
+            String textoDecriptografado= new String(textoDecriptografadoBytes);
+            return;
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         } catch (InvalidKeyException e) {
