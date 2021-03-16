983b0a01aa13a4c7519a95c673d56786954dca54
Yuri Franca dos Anjos <yuri.anjos@cwi.com.br>
2021-03-16 19:56:36 -0300

    api

---
+++ api/src/main/java/br/com/ifsul/trabalhoSI/TrabalhoSiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifsul.trabalhoSI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoSiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoSiApplication.class, args);
+	}
+
+}
---
+++ api/src/main/java/br/com/ifsul/trabalhoSI/controller/Controller.java
@@ -0,0 +1,9 @@
+package br.com.ifsul.trabalhoSI.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/privado/vaquinha")
+public class Controller {
+}
