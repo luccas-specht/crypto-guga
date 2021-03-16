e6152bcc0272ab67b3dc9c5ee43f09334d151af8
Yuri Franca dos Anjos <yuri.anjos@cwi.com.br>
2021-03-16 20:36:44 -0300

    api

--- api/src/main/java/br/com/ifsul/trabalhoSI/controller/Controller.java
+++ api/src/main/java/br/com/ifsul/trabalhoSI/controller/Controller.java
@@ -1,9 +1,20 @@
 package br.com.ifsul.trabalhoSI.controller;
 
+import br.com.ifsul.trabalhoSI.feature.CriptografiaSimetrica;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/privado/vaquinha")
+@RequestMapping("/criptografia")
 public class Controller {
+
+    @Autowired
+    private CriptografiaSimetrica criptografiaSimetrica;
+
+    @GetMapping("/simetrico")
+    public void simetrico(){
+        criptografiaSimetrica.metodo();
+    }
 }
